cd9d651e94f0fa4dbefeb8f328fc9fb59270643f
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-23 08:59:21 +0400

    created files

---
+++ src/main/java/com/PayPal/PayPal/PayPalIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.PayPal.PayPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayPalIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayPalIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PayPal/PayPal/PayPalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PayPal.PayPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
